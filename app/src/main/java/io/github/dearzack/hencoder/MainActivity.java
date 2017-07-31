77b268975a2278511fadec7805106cf43a4e56c6
DearZack <zack4work@gmail.com>
2017-07-31 22:12:42 +0800

    新加一个页面

--- app/src/main/java/io/github/dearzack/hencoder/MainActivity.java
+++ app/src/main/java/io/github/dearzack/hencoder/MainActivity.java
@@ -2,9 +2,11 @@ package io.github.dearzack.hencoder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +19,14 @@ import io.github.dearzack.hencoder.adapter.MainAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private FloatingActionButton fab;
     private List<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         initData();
         recyclerView = (RecyclerView) findViewById(R.id.main_list);
         MainAdapter mainAdapter = new MainAdapter(this, data, new MainAdapter.OnItemClickListener() {
@@ -33,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
         });
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         recyclerView.setAdapter(mainAdapter);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void initData() {
---
+++ app/src/main/java/io/github/dearzack/hencoder/SecondaryActivity.java
@@ -0,0 +1,27 @@
+package io.github.dearzack.hencoder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondaryActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
